a2cf248e485bca47f5075ce78be8ccd6a6fef6d4
sai109 <lukebraithwaite7@gmail.com>
2018-06-01 08:35:03 +0100

    Client added

---
+++ validation/post.js
@@ -0,0 +1,20 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = (data) => {
+	let errors = {};
+
+	data.text = !isEmpty(data.text) ? data.text : '';
+
+	if (!Validator.isLength(data.text, { min: 10, max: 300})) {
+		errors.text = 'Post cannot be longer than 300 character';
+	}
+	if (Validator.isEmpty(data.text)) {
+		errors.text = 'Post cannot be empty';
+	}
+	
+	return {
+		errors,
+		isValid: isEmpty(errors)
+	};
+};
