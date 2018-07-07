640cec5369615ea4b7f994d52a2e413cdc32efe3
sai109 <lukebraithwaite7@gmail.com>
2018-07-07 15:29:21 +0100

    Fixed security problem

--- server/server.js
+++ server/server.js
@@ -34,13 +34,11 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/build')));
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, '../client/build/index.html'));
 	});
-
 }
 
 const port = process.env.PORT || 5000;
