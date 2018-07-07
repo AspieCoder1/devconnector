32b92d5ac5eec9983c074035fec625f82d9dd562
sai109 <lukebraithwaite7@gmail.com>
2018-07-07 14:50:23 +0100

    Prepared for deployment

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const helmet = require('helmet');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -32,6 +33,13 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+if (proccess.env.NODE_ENV) {
+	express.use(express.static(path.join(__dirname, '../client/build')));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, '../client/build/index.html'));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
